b6cb4797374a2c14946bb8a010cb8e2e8d28919b
Александр Сунцов <alex_sun@bk.ru>
2015-02-06 01:17:12 +0300

    index

--- src/Fourxxi/TestJobBundle/Controller/DefaultController.php
+++ src/Fourxxi/TestJobBundle/Controller/DefaultController.php
@@ -5,21 +5,61 @@ namespace Fourxxi\TestJobBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Fourxxi\TestJobBundle\Entity\Message;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
-     * @Route("/")
+     * @Route("/", name="home")
      * @Template()
      */
-    public function indexAction()
-    {
+    public function indexAction() {
         $doctrine = $this->getDoctrine();
         $users = $doctrine->getRepository('FourxxiTestJobBundle:User')->findAll();
+        $curentUserId = $this->getUser()->getId();
         $messages = $doctrine->getRepository('FourxxiTestJobBundle:Message')->findAll();
-        return $this->render('FourxxiTestJobBundle:Default:index.html.twig',array(
-            'users' => $users,
-            'messages' => $messages
+        return $this->render('FourxxiTestJobBundle:Default:index.html.twig', array(
+                    'users' => $users,
+                    'usersid' => $curentUserId,
+                    'messages' => $messages
         ));
     }
+
+    /**
+     * @Route("/message")
+     * @Template()
+     */
+    public function messageAction(Request $request) {
+        $doctrine = $this->getDoctrine();
+        $em = $doctrine->getManager();
+        $repo = $doctrine->getRepository('FourxxiTestJobBundle:Message');
+        $user = $this->getUser();
+        $curentUserId = $user->getId();
+        
+        if ($request->getMethod() !== 'POST') {
+            throw $this->createNotFoundException('POST data not found');
+        }
+        $id = $request->request->get('id', null);
+        $body = $request->request->get('body', null);
+        if (!$body) {
+            throw $this->createNotFoundException('BODY not found');
+        }
+        if ($id) {
+            $message = $repo->find($id);
+        } else {
+            $message = new Message();
+            $message->setUser($user);
+        }
+        $message->setBody($body);
+        $em->persist($message);
+        $em->flush();
+        return $this->render('FourxxiTestJobBundle:Default:message.html.twig', array(
+                    'usersid' => $curentUserId,
+                    'message' => $message
+        ));
+
+    }
+
 }
--- src/Fourxxi/TestJobBundle/Resources/public/js/index.js
+++ src/Fourxxi/TestJobBundle/Resources/public/js/index.js
@@ -1,7 +1,49 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+var messageId = null;
+
+$(function () {
+    $('#save').click(function () {
+        saveMessage(messageId);
+    });
+    $('#savenew').click(function () {
+        saveMessage(null);
+    });
+});
 
 
+
+function newMessage() {    
+    $('#editform').css({display: 'block'});
+    $('#editform>textarea').focus();
+    $('#editform>span').html('New message');
+}
+function editMessage(msgId, afterUpdate) {
+    messageId = msgId;
+    $('#editform>span').html('Edit message #' + messageId);
+    if (!afterUpdate) {
+        $('#editform>textarea').val($('#messagebody-' + messageId).text());
+        $('#editform>textarea').focus();
+    }
+    $('#save').css({display: 'inline-block'});
+}
+
+function saveMessage(msgId) {
+    var data = {};
+    if (msgId) {
+        data.id = msgId;
+    }
+    data.body = $('#body').val();
+    $.post("message", data).done(function (data) {
+        if (msgId) {
+            $('.messagediv[data-messageid=' + msgId + ']').replaceWith(data);
+        } else {
+            newdiv = $(data);
+            $('#messages').append(newdiv);
+            msgId = newdiv.attr('data-messageid');
+            editMessage(msgId, true);
+        }
+//        $('#save').css({display: 'none'});
+//        $('#editform>span').html('New message');
+    });
+    messageId = null;
+}
